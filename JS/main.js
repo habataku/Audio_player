22efc94b2486a5f3ea3689a30f9090c4fc4f2688
PavelSkorikov <spg75@yandex.ru>
2020-01-12 15:31:22 +0300

    add 'Close button' for player

--- JS/main.js
+++ JS/main.js
@@ -21,6 +21,7 @@ open_player.onclick = function() {
 // основная функция плеера
 function player(elem) {
     let audio_player = elem.getElementById('audio_player');
+    audio_player.currentTime = 1;
     let audio_file = elem.getElementById('audio_file');
     // запуск инпута выбора файла
     let file_open = elem.getElementById('file_open');
@@ -31,9 +32,13 @@ function player(elem) {
     audio_file.onchange = function() {
         let files = this.files;
         let file = URL.createObjectURL(files[0]);
+        if (files[0].name.substring(files[0].name.lastIndexOf("."), files[0].name.length) != '.mp3') {
+            alert('неверный формат файла');
+            return
+        };
         elem.getElementById("title").textContent = files[0].name.substring(0, (files[0].name.lastIndexOf(".")));
         audio_player.src = file;
-        play.click();
+            play.click();
     };
     let progress = elem.getElementById('progress')
     progress.value = 1
@@ -48,8 +53,8 @@ function player(elem) {
         current_time = elem.getElementById("current_time"),
         full_time = elem.getElementById("full_time"),
         download = elem.getElementById("download"),
-        upload = elem.getElementById("upload")
-    audio_player.currentTime = 1
+        upload = elem.getElementById("upload"),
+        close = elem.getElementById("close");
 
     //запуск воспроизведения
     play.onclick = function() {
@@ -93,17 +98,17 @@ function player(elem) {
         progress.value = (current / full) * 100
         let color = 'linear-gradient(90deg, #cc64b7 ' + progress.value + '%, #ffff ' + progress.value + '%)'
         progress.style.background = color
-    }
+    };
     // отображение регулятора громкости
     volume.onclick = () => {
         volume_bar.style.display = volume_bar.style.display === 'none' ? 'block' : 'none'
-    }
+    };
     // изменение стиля и громкости воспроизведения в зависимости от положения регулятора
     volume_bar.oninput = () => {
         let color_volume = 'linear-gradient(90deg, #cc64b7 ' + volume_bar.value + '%, #404040 ' + volume_bar.value + '%)'
         volume_bar.style.background = color_volume
         audio_player.volume = volume_bar.value / 100
-    }
+    };
     // скачивание файла
     download.onclick = () => {
         var link = document.createElement("a")
@@ -114,9 +119,13 @@ function player(elem) {
         link.click();
         document.body.removeChild(link);
         delete link;
-    }
+    };
     // имитация загрузки файла
     upload.onclick = () => {
         alert('отправить файл')
+    };
+    // удаляем тег <audio_player>, который является хостом для данного shadowRoot
+    close.onclick = function () {
+        elem.host.remove();
     }
 }
